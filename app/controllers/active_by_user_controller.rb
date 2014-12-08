b40fc98678e9c9786144b2a95ac1e4e1f3eb1705
Jonathan Katon <jonathan.katon@teradyne.com>
2014-12-08 14:57:35 -0500

    Some changes I forgot to push
    * Unfortunately I completely forgot to commit these changes to
    * github but becuase they were made months ago I dont know what
    * they do or if they fix any existing problems.

--- app/controllers/active_by_user_controller.rb
+++ app/controllers/active_by_user_controller.rb
@@ -6,8 +6,99 @@ class ActiveByUserController < ApplicationController
 
 
   def index
-    #@res_custom_field = CustomField.find_by_name("Resolution")
-    #@resolutions = @res_custom_field.possible_values
+    @res_custom_field = CustomField.find_by_name("Resolution")
+    @resolutions = @res_custom_field.possible_values
+    @issues = Issue.find_by_sql("SELECT IFNULL(issues.assigned_to_id,1) as assign_id, IFNULL(custom_values.value, 'None') AS resolution, 
+                                    issue_statuses.name AS status_name, issue_statuses.id AS status_id, 
+                                    COUNT(*) AS count
+                                  FROM issues, issue_statuses, custom_values
+                                  WHERE issue_statuses.name != 'Closed'
+                                  AND issue_statuses.name != 'Rejected'
+                                  AND issues.status_id = issue_statuses.id
+                                  AND custom_values.customized_type = 'Issue'
+                                  AND custom_values.custom_field_id = #{@res_custom_field.id}
+                                  AND custom_values.customized_id = issues.id
+                                  GROUP BY assign_id, status_name, resolution
+                                  ORDER BY assign_id, status_id")
+    # Build array of rows to display
+    @rows = []
+    cur_user = @issues.first.assign_id
+    cur_stat = @issues.first.status_name
+    count = 0
+    temp_rows = []
+    temp_row = Array.new(@resolutions.count + 3){|c| 0}
+    sum_row = Array.new(@resolutions.count + 3){|c| 0}
+    sum_row[0] = ""
+    sum_row[1] = ""
+    @issues.each do |i|
+      # if issue is different user than last then
+      # create row with last users name 
+      # then push all temp_rows (each status row) into the @rows array
+      # this user now complete so clear count, and temp_rows
+      # and reset cur_user to the new user
+      if (i.assign_id != cur_user)
+        row = Array.new(@resolutions.count + 3)
+        u = User.find(cur_user)
+	name = "Unassigned"
+	if cur_user != 1
+	  name = u.firstname + " " + u.lastname
+	end
+        row[0] = name
+        row[1] = temp_rows.length + 2
+        @rows << row
+        temp_row[0] = ""
+        temp_row[1] = cur_stat
+        temp_row[temp_row.length-1] += count
+        sum_row[temp_row.length-1] += count
+        temp_rows << temp_row
+        temp_rows.each do |t|
+          @rows << t
+        end
+        @rows << sum_row
+        temp_row = Array.new(@resolutions.count + 3){|c| 0}
+        sum_row = Array.new(@resolutions.count + 3){|c| 0}
+        sum_row[0] = ""
+        sum_row[1] = ""
+        temp_rows = []
+        cur_stat = i.status_name
+        count = 0
+        cur_user = i.assign_id
+      # if on same user but different status then push current status row into temp rows array
+      # and reinitialize the temp_row array
+      elsif (i.status_name != cur_stat)
+        logger.debug temp_row
+        temp_row[0] = ""
+        temp_row[1] = cur_stat
+        temp_row[temp_row.length-1] += count
+        sum_row[temp_row.length-1] += count
+        temp_rows << temp_row
+        temp_row = Array.new(@resolutions.count + 3){|c| 0}
+        cur_stat = i.status_name
+        count = 0
+      end
+      # increment count by issue count and push count in correct resolution column
+      count += i.count
+      res = (i.resolution == "") ? "None" : i.resolution
+      temp_row[@resolutions.index(res) + 2] += i.count
+      sum_row[@resolutions.index(res) + 2] += i.count
+    end
+    row = Array.new(@resolutions.count + 3)
+    u = User.find(cur_user)
+    row[0] = u.firstname + " " + u.lastname
+    row[1] = temp_rows.length + 2
+    @rows << row
+    temp_row[0] = ""
+    temp_row[1] = cur_stat
+    temp_row[temp_row.length-1] += count
+    sum_row[temp_row.length-1] += count
+    temp_rows << temp_row
+    temp_rows.each do |t|
+      @rows << t
+    end
+    @rows << sum_row
+
+
+
     #@statuses = IssueStatus.find_all_by_is_closed(false)
     #@issues = Issue.find(:all, :conditions => ["status_id IN (?)", @statuses])
     #@statuses = IssueStatus.find_by_sql("SELECT GROUP_CONCAT(id SEPARATOR ',') FROM `issue_statuses` WHERE is_closed = false")
